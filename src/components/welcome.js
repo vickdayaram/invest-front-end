c5bbf880648215bd035ed057e64cdd7c4ab00095
github username <github email address>
2017-08-14 14:51:30 -0400

    got the pie chart working, have not figured out a good color scheme just yet. will work on styling

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,15 +1,43 @@
 import React from 'react'
+import { Doughnut } from 'react-chartjs-2'
+
+const sample =  {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(60, 180, 75)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    }
+
+    const chartOptions = {
+      // Elements options apply to all of the options unless overridden in a dataset
+      // In this case, we are setting the border of each bar to be 2px wide and green
+      maintainAspectRatio: false,
+      title:{
+        display: true,
+        text: "Current Allocation",
+        fontSize: 25,
+        position: "top",
+        fontColor: "black"
+      }
+    }
 
 class Welcome extends React.Component {
 
-  constructor(props){
-    super(props)
+  state = {
+    chartData: {}
   }
+
+
+
 
   render(){
     return(
       <div className="welcome">
         Welcome {this.props.current_user}, Portfolio Total: ${parseInt(this.props.portfolioTotal).toLocaleString()}
+        < Doughnut data={this.props.chartData} options={chartOptions} height={250} width={250}/>
       </div>
     )
   }
--- src/containers/appContainer.js
+++ src/containers/appContainer.js
@@ -10,7 +10,8 @@ class AppContainer extends React.Component {
   state = {
     current_user: "",
     portfolioTotal: 0,
-    currentAllocation: {}
+    currentAllocation: {},
+    chartData: {}
   }
 
   componentDidMount = () => {
@@ -20,13 +21,48 @@ class AppContainer extends React.Component {
       portfolioTotal: json["portfolio_total"],
       currentAllocation: json["allocation"]
     }))
+    .then(() => this.formatChartData())
+  }
+
+  formatChartData = () => {
+    let rawData = this.state.currentAllocation
+    let labels = []
+    let data = []
+    let backgroundColor = []
+    let label = ""
+    let value = ""
+    for(let i = 0; i < rawData.length; i++){
+      let label = Object.keys(rawData[i]).pop()
+      let value = Object.values(rawData[i]).pop().toFixed(2)
+      labels.push(label)
+      data.push(value)
+      if(label === "MM"){
+        backgroundColor.push('#E0E1DD')
+      } else if(label === "VTI" || label === "VXUS"){
+        backgroundColor.push('#0D1B2A')
+      } else {
+        backgroundColor.push('#0D3B66')
+      }
+    }
+    let chartData  =  {
+                labels: labels,
+                datasets: [{
+                label: "Portfolio Allocation",
+                backgroundColor: backgroundColor,
+                borderColor: '#000',
+                data: data,
+            }]
+        }
+    this.setState({
+      chartData: chartData
+    })
   }
 
   render(){
     return (
       <div className="home">
 
-          < Welcome current_user={this.state.current_user} portfolioTotal={this.state.portfolioTotal} currentAllocation={this.state.currentAllocation}/>
+          < Welcome current_user={this.state.current_user} portfolioTotal={this.state.portfolioTotal} chartData={this.state.chartData}/>
           < Accounts  />
 
 

334c9bc2c2f55a21caac633b0220e79affb00b14
github username <github email address>
2017-08-17 22:59:13 -0400

    switched url to heroku to deploy

--- src/authAdapter.js
+++ src/authAdapter.js
@@ -1,6 +1,6 @@
 const localhost = 'http://localhost:3000/api/v1'
 const herokuUrl = 'https://hidden-sea-22172.herokuapp.com/api/v1'
-let baseUrl = localhost
+let baseUrl = herokuUrl
 
 export default class AuthAdapter {
   static login (loginParams) {

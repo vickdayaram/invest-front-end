78cc4f953ba4a3128d95108b1dc0a1115959c456
github username <github email address>
2017-08-23 19:02:13 -0400

    updated some wording for the recommendation modal

--- src/components/recommendationmodal.js
+++ src/components/recommendationmodal.js
@@ -176,15 +176,13 @@ class RecommendationModal extends Component {
           <h3>Interested in implementing this portfolio? </h3>
           <p>If you are interested in implementing this portfolio, click on the</p>
           <p>Proceed to implement button below.</p>
-          <p>All investment details come from Vanguard.coms website.</p>
-          <p>This recommendation do not substitute an over all financial planning assesment.</p>
-          <p>If are uncertain about any of this please seek the help of a financial advisor.</p>
+          <p>All investment details come from the Vanguard website.</p>
+          <p>This recommendation does not substitute an over all financial planning assesment.</p>
+          <p>If are uncertain about your finances and have additional questions, </p>
+          <p>seek the help of a financial advisor.</p>
           <h3>Important Notice</h3>
-          <h3>You will not have trading access to this account!! it is intended to </h3>
+          <h3>You will not have trading access to this account. it is intended to </h3>
           <h3>grow indefinitly and show you how an index based approach can work. </h3>
-          <h3>All investing is subject to risk, including the possible </h3>
-          <h3>loss of the money you invest.</h3>
-          <h3>Diversification does not ensure a profit or protect against a loss.</h3>
         </div>
       </Modal.Description>
     </Modal.Content>
